575a800137745217dd3283381f40df5f9944a315
@hexlet/cli <support@hexlet.io>
2024-06-08 01:48:18 +0300

    submit java-web-ru/validation

---
+++ java-web-ru/validation/src/main/java/exercise/App.java
@@ -0,0 +1,73 @@
+package exercise;
+
+import io.javalin.Javalin;
+import io.javalin.validation.ValidationException;
+import java.util.List;
+import exercise.model.Article;
+import exercise.dto.articles.ArticlesPage;
+import exercise.dto.articles.BuildArticlePage;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import io.javalin.rendering.template.JavalinJte;
+
+import exercise.repository.ArticleRepository;
+import java.util.Collections;
+
+
+public final class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        app.get("/articles", ctx -> {
+            List<Article> articles = ArticleRepository.getEntities();
+            var page = new ArticlesPage(articles);
+            ctx.render("articles/index.jte", model("page", page));
+        });
+
+        // BEGIN
+        app.post("/articles", ctx -> {
+            String title;
+            String content;
+
+            try {
+                title = ctx.formParamAsClass("title", String.class)
+                        .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
+                        .check(value -> !ArticleRepository.existsByTitle(value), "Статья с таким названием уже существует")
+                        .get();
+                content = ctx.formParamAsClass("content", String.class)
+                        .check(c -> c.length() >= 10, "Статья должна быть не короче 10 символов")
+                        .get();
+                var article = new Article(title, content);
+                ArticleRepository.save(article);
+                ctx.redirect("/articles");
+            } catch (ValidationException e) {
+                title   = ctx.formParam("title");
+                content = ctx.formParam("content");
+                var page = new BuildArticlePage(title, content, e.getErrors());
+                ctx.status(422);
+                ctx.render("articles/build.jte", Collections.singletonMap("page", page));
+            }
+        });
+
+        app.get("/articles/build", ctx -> {
+            var page = new BuildArticlePage();
+            ctx.render("articles/build.jte", Collections.singletonMap("page", page));
+        });
+        // END
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
