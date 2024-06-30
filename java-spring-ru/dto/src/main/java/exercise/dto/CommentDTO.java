5475e91ac0e37209b4056329d4a4a68776c015d2
@hexlet/cli <support@hexlet.io>
2024-06-30 16:12:16 +0300

    submit java-spring-ru/dto

---
+++ java-spring-ru/dto/src/main/java/exercise/dto/CommentDTO.java
@@ -0,0 +1,14 @@
+package exercise.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+// BEGIN
+@Getter
+@Setter
+public class CommentDTO {
+    private Long   id;
+    private String body;
+}
+// END
---
+++ java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
@@ -0,0 +1,17 @@
+package exercise.dto;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+
+// BEGIN
+@Setter
+@Getter
+public class PostDTO {
+    private Long             id;
+    private String           title;
+    private String           body;
+    private List<CommentDTO> comments;
+}
+// END
